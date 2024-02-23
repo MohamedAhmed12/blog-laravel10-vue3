c396360911a31f032a71e5ce2b21b2712f4e0c95
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-23 20:59:58 +0200

    feat(blog-client):  dashboard blog list route

---
+++ client/src/layouts/DashboardLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore();
+
+const user = computed(() => store.getters["auth/getUser"]);
+console.log(user);
+</script>
+
+
+<template>
+  <div>
+    <nav>
+      <router-link to="/dashboard">Home</router-link>
+      <router-link to="/dashboard/settings">Settings</router-link>
+      <router-link to="/dashboard/profile">Profile</router-link>
+    </nav>
+    {{ user }}
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+/* Component styles */
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,7 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Login from "../views/Login.vue";
+import Login from "../views/auth/Login.vue";
+import BlogList from "../views/dashboard/blogs/BlogList.vue";
+import DashboardLayout from "../layouts/DashboardLayout.vue";
+import { authMiddleware } from "@/middlewares/authMiddleware";
 
-const routes = [{ path: "/auth/login", component: Login }];
+const routes = [
+  {
+    path: "/auth/login",
+    component: Login,
+  },
+  {
+    path: "/dashboard",
+    component: DashboardLayout,
+    beforeEnter: authMiddleware,
+    children: [
+      {
+        path: "blogs",
+        component: BlogList,
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
