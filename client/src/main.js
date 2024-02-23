c6cd2775da35d0c86f9cc26af53e7348061bc42d
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-23 08:19:31 +0200

    feat(client-vuetify): implement vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,17 @@
+import "vuetify/styles";
 import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
 import App from "./App.vue";
-import router from './router';
+import router from "./router";
 
-const vuetify = createVuetify();
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 const app = createApp(App);
 app.use(router);
