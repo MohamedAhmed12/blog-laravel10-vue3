d2b89ab09b77d09edc1f0edd1a92a36a857acbd0
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-23 21:04:45 +0200

    feat(axios): use interceptors to add Authorization header if store has token

--- client/src/plugins/axios.js
+++ client/src/plugins/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import store from "../store";
 
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_APP_SERVER_URL}/api/`,
@@ -7,5 +8,20 @@ const axiosInstance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+axiosInstance.interceptors.request.use(
+  function(config) {
+    const token = store.getters["auth/getToken"];
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
