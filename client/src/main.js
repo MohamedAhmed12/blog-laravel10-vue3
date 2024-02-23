a38cc786ff80191c352b1f8664c3a922eb19b83f
MohamedAhmed12 <mohamed7el17@gmail.com>
2024-02-23 07:43:22 +0200

    feat(router): implement login router using vue-router

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,11 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createVuetify } from "vuetify";
 import App from "./App.vue";
+import router from './router';
 
 const vuetify = createVuetify();
 
 const app = createApp(App);
+app.use(router);
 app.use(vuetify);
 app.mount("#app");
